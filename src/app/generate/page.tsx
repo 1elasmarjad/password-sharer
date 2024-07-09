4c523c942f4a9b832526f7c834d55fad964a25ac
1elasmarjad <jadelasmar4@gmail.com>
2024-07-09 12:25:48 -0400

    🔒 /generate login page created

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,3 +1,5 @@
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 import OTPBlock from "~/components/ui/password-block";
 import { validateRequest } from "~/server/actions/auth";
 
@@ -6,7 +8,17 @@ export default async function GeneratePage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-[#212429]">
-      <OTPBlock authContext={authContext} />
+      {authContext.user ? (
+        <OTPBlock authContext={authContext} />
+      ) : (
+        <div className="bg-[#222222]  flex flex-col px-16 py-12 items-center gap-4">
+          <h1 className="text-xl text-gray-400">You Must Login</h1>
+
+          <Link href="/login/discord" className="rounded border-2 border-gray-700 pt-2 pb-2.5 text-lg tracking-widest text-[#77B9EE] transition-all hover:bg-gray-700 px-6 flex items-center justify-center gap-2">
+            Discord Login <LogIn className="h-4 w-4 mt-1"/>
+          </Link>
+        </div>
+      )}
     </main>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,6 @@ import Combobox from "~/components/ui/combobox";
 import { generateCode } from "~/server/actions/passwords";
 
 export default function HomePage() {
-  // const code = createCode({ userId: "123" });
-
   return (
     <main className="flex min-h-screen flex-col">
       <Link href="/login/discord">Test</Link>
